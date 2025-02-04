0e6d3774d2a10ccb3b88560212f6d2dde5d8e4dd
21f1006385 <21f1006385@ds.study.iitm.ac.in>
2025-02-05 03:48:29 +0530

    Backend parts

---
+++ backend/controllers.py
@@ -0,0 +1,13 @@
+# App routes
+from flask import Flask,render_template
+from flask import current_app as app
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+# Many controllers/routers here
+
